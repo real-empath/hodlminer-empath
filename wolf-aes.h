bb68340c607d7ec699ff4da93665b13b808704af
Optiminer <optiminer@tutamail.com>
2016-07-31 17:17:26 +0200

    Optimize AES and add new SHA512 code that uses AVX/AVX2.

--- wolf-aes.h
+++ wolf-aes.h
@@ -4,7 +4,10 @@
 #include <stdint.h>
 #include <x86intrin.h>
 
+#define AES_PARALLEL_N 8
+#define BLOCK_COUNT 256
+
 void ExpandAESKey256(__m128i *keys, const __m128i *KeyBuf);
-void AES256CBC(__m128i *Ciphertext, const __m128i *Plaintext, const __m128i *ExpandedKey, __m128i IV, uint32_t BlockCount);
+void AES256CBC(__m128i** data, const __m128i** next, const __m128i ExpandedKey[][16], __m128i* IV);
 
 #endif		// __WOLF_AES_H
